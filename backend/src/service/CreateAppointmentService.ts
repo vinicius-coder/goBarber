0949d33cd377aed732306812353a5e3976a68226
Avonts <vinialves022@gmail.com>
2021-03-26 11:23:10 -0300

    Add Post users

--- backend/src/service/CreateAppointmentService.ts
+++ backend/src/service/CreateAppointmentService.ts
@@ -4,13 +4,13 @@ import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 interface Request {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
 
-    public async execute({ provider, date }: Request): Promise<Appointment> {
+    public async execute({ provider_id, date }: Request): Promise<Appointment> {
 
         const appointmentRepository = getCustomRepository(AppointmentsRepository);
 
@@ -23,7 +23,7 @@ class CreateAppointmentService {
         }
 
         const appointment = appointmentRepository.create({
-            provider,
+            provider_id,
             date: appointmentDate,
         });
 
