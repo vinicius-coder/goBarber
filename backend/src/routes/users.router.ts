c8dde0db3bfe5a9cb657f0cbc48e9b68ea5281b7
Avonts <vinialves022@gmail.com>
2021-03-29 22:14:26 -0300

    Fix upload avatar

--- backend/src/routes/users.router.ts
+++ backend/src/routes/users.router.ts
@@ -5,6 +5,7 @@ import { getRepository } from 'typeorm';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import User from '../models/Users';
 import CreateUserService from '../service/CreateUserService';
+import UpdateUserAvatarService from '../service/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig)
@@ -37,7 +38,7 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-    
+
     const usersRepository = getRepository(User);
 
     const users = await usersRepository.find();
@@ -46,12 +47,31 @@ usersRouter.get('/', async (request, response) => {
 
 })
 
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response, ) => {
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response,) => {
 
-      
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService;
 
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        })
 
-    return response.json(true);
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
+
+        return response.json(userWithoutPassword);
+
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+
 })
 
 export default usersRouter;
